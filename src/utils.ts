4462969983a2ecaa403332f680a15197adb4de28
DanielZlotin <zlotindaniel@gmail.com>
2021-05-27 16:24:27 +0300

    initial

---
+++ src/utils.ts
@@ -0,0 +1,83 @@
+import Web3 from "web3";
+import BN from "bn.js";
+import _ from "lodash";
+
+export const zero = bn("0");
+export const ether = bn18("1");
+export const max = "115792089237316195423570985008687907853269984665640564039457584007913129639935";
+
+export function bn(n: BN | string | number): BN {
+  if (!n) return zero;
+  return new BN(n, 10);
+}
+
+/**
+ * assuming 18 decimals, uncommify (support "1,000")
+ */
+export function bn18(n: string): BN {
+  return bn(Web3.utils.toWei(n.split(",").join(""), "ether"));
+}
+
+/**
+ * assuming 9 decimals (gwei), uncommify (support "1,000")
+ */
+export function bn9(n: string): BN {
+  return bn(Web3.utils.toWei(n.split(",").join(""), "gwei"));
+}
+
+/**
+ * assuming 8 decimals, uncommify (support "1,000")
+ */
+export function bn8(n: string): BN {
+  return bn9(n).divn(10);
+}
+
+/**
+ * assuming 6 decimals, uncommify (support "1,000")
+ */
+export function bn6(e: string): BN {
+  return bn(Web3.utils.toWei(e.split(",").join(""), "lovelace"));
+}
+
+/**
+ * formats from wei, assuming 18 decimals
+ */
+export function fmt18(ether: BN | number | string): string {
+  return commify(Web3.utils.fromWei(bn(ether), "ether"));
+}
+
+/**
+ * formats from wei, assuming 9 decimals
+ */
+export function fmt9(ether: BN | number | string): string {
+  return commify(Web3.utils.fromWei(bn(ether), "gwei"));
+}
+
+/**
+ * formats from wei, assuming 8 decimals
+ */
+export function fmt8(n: BN | number | string): string {
+  return fmt9(bn(n).muln(10));
+}
+
+/**
+ * formats from wei, assuming 6 decimals
+ */
+export function fmt6(ether: BN | number | string): string {
+  return commify(Web3.utils.fromWei(bn(ether), "lovelace"));
+}
+
+function commify(num: string) {
+  const parts = _.split(num, ".");
+  const upper = _.chain(parts[0].split(""))
+    .reverse()
+    .chunk(3)
+    .map((c) => c.reverse().join(""))
+    .reverse()
+    .join(",")
+    .value();
+
+  const lower = parts[1]; //_.chain(parts[1]).padEnd(4, "0").truncate({ length: 4, omission: "" }).value();
+  if (lower) return `${upper}.${lower}`;
+  else return upper;
+}
---
+++ test/utils-test.ts
@@ -0,0 +1,51 @@
+import { expect, use } from "chai";
+import { bn, bn18, bn6, bn8, bn9, ether, fmt18, fmt6, fmt8, fmt9, max, zero } from "../src/utils";
+import BN from "bn.js";
+import CBN from "chai-bn";
+
+before(() => {
+  use(CBN(BN));
+});
+
+describe("utils", () => {
+  it("bn", async () => {
+    expect(bn(1).toString()).eq("1");
+    expect(bn(1)).bignumber.eq("1");
+
+    expect(bn18("1")).bignumber.eq("1000000000000000000");
+    expect(bn6("1").toString()).eq("1000000");
+    expect(bn6("1")).bignumber.eq("1000000");
+    expect(bn6("0.1")).bignumber.eq("100000");
+
+    expect(bn8("1").toString()).eq("100000000");
+    expect(bn8("1")).bignumber.eq(bn("100000000")).gt(bn6("1"));
+
+    expect(bn9("1").toString()).eq("1000000000");
+    expect(bn9("1")).bignumber.eq(bn("1000000000")).gt(bn6("1"));
+  });
+
+  it("uncommify before parsing", async () => {
+    expect(bn18("1,000,000.0")).bignumber.eq(bn18("1000000"));
+    expect(bn9("1,000,000.0")).bignumber.eq(bn9("1000000"));
+    expect(bn8("1,000,000.0")).bignumber.eq(bn8("1000000"));
+    expect(bn6("1,000,000.0")).bignumber.eq(bn6("1000000"));
+  });
+
+  it("format human readable", async () => {
+    expect(fmt6(bn6("1"))).eq("1");
+    expect(fmt8(bn8("1"))).eq("1");
+    expect(fmt9(bn9("1"))).eq("1");
+    expect(fmt18(bn18("1"))).eq("1");
+
+    expect(fmt18(bn18("1,234,567,890.123456789123456789"))).eq("1,234,567,890.123456789123456789");
+    expect(fmt9(bn9("1,234,567,890.123456789"))).eq("1,234,567,890.123456789");
+    expect(fmt8(bn8("1,234,567,890.12345678"))).eq("1,234,567,890.12345678");
+    expect(fmt6(bn6("1,234,567,890.123456"))).eq("1,234,567,890.123456");
+  });
+
+  it("constants", async () => {
+    expect(zero).bignumber.eq(bn(0)).eq("0");
+    expect(ether).bignumber.eq(bn18("1"));
+    expect(max).bignumber.eq(bn("2").pow(bn("256")).subn(1)); //max 256 bytes value
+  });
+});
