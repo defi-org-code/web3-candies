5249a3a508b991795b25433c39f9e3b11364baea
DanielZlotin <zlotindaniel@gmail.com>
2021-05-30 23:35:42 +0300

    temp dist

--- dist-temp/hardhat.config.d.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig } from "hardhat/types";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "hardhat-tracer";
-import "@nomiclabs/hardhat-web3";
-import "@nomiclabs/hardhat-etherscan";
-export declare function configFile(): any;
-declare const config: HardhatUserConfig;
-export default config;
