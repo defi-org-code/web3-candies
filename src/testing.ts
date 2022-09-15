b2e0fc676919041ac76ab3c51f36630ccc12e915
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-09-15 20:38:26 +0300

    expectRevert RegExp

--- src/testing.ts
+++ src/testing.ts
@@ -7,14 +7,25 @@ export function useChaiBN() {
   use(CBN(BN));
 }
 
-export async function expectRevert(fn: () => any, withError: string) {
-  expect(withError.trim(), "must provide expected error message").to.not.be.empty;
+export async function expectRevert(fn: () => any, withError: string | RegExp) {
+  if (typeof withError === "string") {
+    expect(withError.trim(), "must provide expected error message").to.not.be.empty;
+  } else {
+    expect(withError.source, "must provide expected error message").to.not.be.empty;
+  }
+
   let err: Error | null = null;
   try {
     await fn();
   } catch (e: any) {
     err = e;
   }
+
   expect(!!err, `expected to revert with '${withError}'`).to.be.true;
-  expect(err!.message, `expected error message:\n\t'${withError}'\n\tbut was:\n\t'${err!.message}'\n\t`).to.include(withError);
+
+  if (typeof withError === "string") {
+    expect(err!.message).to.include(withError, `expected error message:\n\t'${withError}'\n\tbut was:\n\t'${err!.message}'\n\n\t`);
+  } else {
+    expect(err!.message).to.match(withError, `expected error to match:\n\t'${withError}'\n\tbut was:\n\t'${err!.message}'\n\n\t`);
+  }
 }
