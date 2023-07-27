b6667c9bf999405e323522a3b2ee3d51e5e70c95
DanielZlotin <danielzlotin@users.noreply.github.com>
2023-07-27 15:21:34 +0300

    solc

--- src/hardhat/index.ts
+++ src/hardhat/index.ts
@@ -178,11 +178,11 @@ export function hardhatDefaultConfig() {
     solidity: {
       compilers: [
         {
-          version: "0.8.18",
+          version: "0.8.19",
           settings: {
             optimizer: {
               enabled: true,
-              runs: 200,
+              runs: 1_000_000,
             },
           },
         },
